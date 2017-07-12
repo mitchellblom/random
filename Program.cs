e6beb1b9453c83899353fff89399c51f26d916d4
Mitchell Blom <mitchellblom@gmail.com>
2017-07-12 16:20:23 -0500

    number manipulations complete

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace random
 {
@@ -6,7 +7,47 @@ namespace random
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            // generate a list of 20 random numbers that are in the range of 1-50.
+
+            Random random = new Random();
+
+            // Create a list
+
+            List<int> randomNumbers = new List<int>();
+            List<int> squaredNumbers = new List<int>();
+            List<int> squaredEvenNumbers = new List<int>();
+
+
+            // Populate the list
+
+            for (int i = 0; i < 10; i++) {
+                randomNumbers.Add(random.Next(1, 20));
+            }
+    
+            // With the resulting List, populate a new List that contains each number squared. For example, if the original list is `2, 5, 3, 15`, the final list will be `4, 25, 9, 225`.
+            
+            Console.WriteLine("Random Numbers: ");
+            foreach (int number in randomNumbers) {
+                Console.WriteLine(number);
+                squaredNumbers.Add(number * number);
+            }
+
+            // Then remove any number that is odd from the list of squared numbers.
+
+            Console.WriteLine("Squared Random Numbers: ");
+            foreach (int number in squaredNumbers) {
+                Console.WriteLine(number);
+                if (number%2 == 0) {
+                    squaredEvenNumbers.Add(number);
+                }
+            }
+
+            Console.WriteLine("Squared Even Random Numbers: ");
+            foreach (int number in squaredEvenNumbers) {
+                Console.WriteLine(number);
+            }
+
         }
     }
 }
